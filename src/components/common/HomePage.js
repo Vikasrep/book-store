9f8601b1f2bc0c4f01dd744054ce7327e34d5988
Vikas Chaubey <C_Chaubey.Vikas@cerner.com>
2020-05-04 14:18:00 -0500

    commit 1

---
+++ src/components/common/HomePage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Home = () => {
+	return (
+		<div>
+		<h1>Home Page </h1>
+		
+		<p>This is place to buy books!</p>
+		</div>
+		
+	);
+	
+};
+
+export default Home;
